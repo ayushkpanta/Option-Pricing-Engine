b3b7bf75f13a07ac11c2446aed517b562a5fb23c
Ayush Panta <ayushkpanta@gmail.com>
2025-01-25 10:53:18 -0800

    Accurate Binomial Model

--- binomial.cpp
+++ binomial.cpp
@@ -9,9 +9,6 @@ BinomialModel::BinomialModel() {
     std::cout << "Initialized Binomial Model.\n";
 }
 
-// DOES NOT MATCH MY PYTHON IMPLEMENTATION
-// NEED TO DEBUG
-
 double BinomialModel::price_contract(double S, double X, double sigma, double r, double T, double N, std::string type) {
 
     // Get timesep and discount rate
@@ -24,46 +21,30 @@ double BinomialModel::price_contract(double S, double X, double sigma, double r,
     double p_u = (std::exp(r * dt) - d) / (u - d);
     double p_d = 1 - p_u;
 
-    // Get N as size for idiomatic iterating and vector sizing
-    size_t N_spec = static_cast<size_t>(N);
-
-    // Creating 'decay' vector
-    std::vector<double> decay_vec(N_spec+1, 0.0);
-    for (size_t i = 1; i < N_spec; i++) {
-        int decay_factor = N_spec + 1 - i;
-        decay_vec[i] = std::pow(d, decay_factor);
+    // Computing stock prices at maturity
+    std::vector<double> stock_prices(N + 1, 0.0);
+    for (int i = 0; i <= N; i++) {
+        stock_prices[i] = S * std::pow(d, N-i) * std::pow(u, i);
     }
 
-    // Creating 'growth' vector
-    std::vector<double> growth_vec(N_spec+1, 0.0);
-    for (size_t i = 0; i < N_spec+1; i++) {
-        int growth_factor = i;
-        growth_vec[i] = std::pow(u, growth_factor);
-    }
-
-    // Shrinking vectors for efficiency
-    decay_vec.shrink_to_fit();
-    growth_vec.shrink_to_fit();
-
-    // Getting contract valuation vector
-    std::vector<double> contract_value(decay_vec.size(), 0.0);
-    for (int i = 0; i < decay_vec.size(); i++) {
-        double base_value = S * decay_vec[i] * growth_vec[i];
-
+    // Computing options prices at maturity
+    std::vector<double> contract_value(N+1, 0.0);
+    for (int i = 0; i <= N; i++) {
         // Spot - Strike if CALL, flip if PUT
         if (type == "CALL") {
-            contract_value[i] = std::max(base_value - X, 0.0);
+            contract_value[i] = std::max(stock_prices[i] - X, 0.0);
         } else if (type == "PUT") {
-            contract_value[i] = std::max(X - base_value, 0.0);
+            contract_value[i] = std::max(X - stock_prices[i], 0.0);
         }
     }
 
-    // Backwards induction
-    for (size_t i = N_spec - 1; i >= 1; i--) {
-        for (size_t j = 0; j < i; j++) {
+    // Backwards induction through tree
+    for (int i = N - 1; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
             contract_value[j] = discount_rate * (p_u * contract_value[j+1] + p_d * contract_value[j]);
         }
     }
 
+    // Return contract value at the root of the binomial tree
     return contract_value[0];
 }
