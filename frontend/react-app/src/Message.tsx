690e3cff8c178c3e6e7207b3d646448f36bf2525
Ayush Panta <ayushkpanta@gmail.com>
2025-01-25 23:54:59 -0800

    Configuring front-end

---
+++ frontend/react-app/src/Message.tsx
@@ -0,0 +1,17 @@
+
+// Remember to use Pascal casing
+function Title() {
+
+    // JSX: JavaScript XML (conversion)
+    return <h1>Options Pricing Engine</h1>;
+
+}
+
+function Parameters() {
+
+    return <h1>Enter Parameters</h1>;
+
+}
+
+
+export {Parameters, Title};
